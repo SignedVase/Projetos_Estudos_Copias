c8c8e6ef6225982284d2702c4921bd9162d5fa4b
Augusto Alves <augustoalves.07@outlook.com.br>
2026-06-09 23:52:35 -0300

    The beggining of the real code - __init__

--- TheAlgorithms/audio_filters/iir_filter.py
+++ TheAlgorithms/audio_filters/iir_filter.py
@@ -14,10 +14,22 @@ class IIRFilter:
 
     Using the following transfer function
         .. math:: H(z)=\frac(b_{0}=b_{1}z^{-1}+b_{2}z^{-2}+...+b_{k}z^{-k}}
-                  {a_{0}+a_{1}z^{-1}+a_{2}z^{-2}+...+a_{k}z^{-}}
+                  {a_{0}+a_{1}z^{-1}+a_{2}z^{-2}+...+a_{k}z^{-}i
 
     we can rewrite this to
        .. math:: y[n]={\frac{1}{a_{0}}}
                  \left(\left(b_{0}x[n]+b_{1}x[n-1]+b_{2}x[n-2]+...+b_{k}x[n-k]\right)-
-                 \left(a_{1}y[n-1]
+                 \left(a_{1}y[n-1]+a_{2}y[n-2]+...+a_{k}y[n-k]\right)\right)
     """
+    def __init__(self, order: int) -> None:
+        self.order = order
+
+        #a_{0}...a_{k}
+        self.a_coeffs = [1.0] + [0.0] * order
+        #b_{0}...b_{k}
+        self.b_coeffs = [1.0] + [0.0] * order
+
+        #x[n-1]...x[n-k]
+        self.input_hisroty = [0.0] * self.order
+        #y[n-1]...y[n-k]
+        self.output_history = [0.0] * self.order
